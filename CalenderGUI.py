933438a7b87332529a6a0365724ccd98487e1a56
Vishal Patel <vppatel@ualberta.ca>
2016-12-29 20:00:24 -0700

    Established basic input/output
    Incomplete, proof of concept of input and output of calendar save data.

--- CalenderGUI.py
+++ CalenderGUI.py
@@ -1,8 +1,46 @@
 from tkinter import *
+from functools import partial
+
+def callback(day):
+    # print("oi")
+
+    f = open('CalendarSaveData', 'r')
+
+    counter = 0
+    for line in f:
+        if day == counter:
+            print(line)
+        counter += 1
+
+    # print(day)
+
+    top = Toplevel()
+    event = Entry(top)
+    event.pack()
+
+    enter = Button(top, text="Enter", command=lambda day=day, event=event: enterGoal(event, day))
+    enter.pack()
+
+    f.close()
+
+def enterGoal(event, day):
+
+    with open('CalendarSaveData') as fin, open('test', 'w') as fout:
+        s = event.get()
+        print(s)
+
+        counter = 0
+        for line in fin:
+
+            if day == counter:
+                output = s + "\n"
+                fout.write(output)
+            else:
+                fout.write(line)
+
+            counter += 1
 
 
-def callback():
-    print("oi")
 
 def notcallback():
     print("lack of oi")
@@ -10,14 +48,21 @@ def notcallback():
 root = Tk()
 
 day = []
+calendarRowShift = 1
+lengthOfCalendar = 5
+widthOfCalendar = 7
+month = "January"
+
+monthTitle = Label(root, text=month)
+monthTitle.grid(row=0, columnspan=widthOfCalendar)
 
 for i in range(31):
-    day.append(Button(root, text=i+1, command=callback))
+    day.append(Button(root, text=i+1, command=lambda i=i: callback(i), height=5, width=8))
 
 counter = 0
 for x in range(5):
     for y in range(7):
-        day[counter].grid(row=x, column=y)
+        day[counter].grid(row=x+calendarRowShift, column=y)
         counter += 1
         if counter == 31:
             break
